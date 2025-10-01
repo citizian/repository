150eff87d4e99be1502be477da01d165f756d3f2
yang zi an <yzachn@163.com>
2025-10-01 17:21:54 +0800

    commit

--- cpp-mini-webserver/www/script.js
+++ cpp-mini-webserver/www/script.js
@@ -1,3 +1,5 @@
-document.getElementById("clickBtn").addEventListener("click", function() {
-    document.getElementById("output").innerText = "Button clicked! Hello from JS.";
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("clickBtn").addEventListener("click", function() {
+        document.getElementById("output").innerText = "Button clicked! Hello from JS.";
+    });
 });
